7594beba180c853a69f055a92d707f3bd4c11d1b
thinhndd <thinhndd@nec.vn>
2025-07-24 17:50:00 +0700

    add db

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ app.use(morgan("dev"));
 app.use(helmet());
 app.use(compression());
 // init db
-
+require("./dbs/init.mongodb");
 // init routes
 app.get("/", (req, res, next) => {
   return res.status(200).json({
